ae44ebf180aecb21fe897fd11bf26d0414aebf20
AbhinnFromIndia <vermaabhinn@gmail.com>
2025-03-23 11:25:54 +0530

    Add: Json format test for api purposes

--- src/utils/model_loader.py
+++ src/utils/model_loader.py
@@ -10,7 +10,7 @@ _model = None
 def load_model():
     """Loads and returns the trained model (lazy-loaded)."""
     global _model
-    if _model is None:  # Load only if not already loaded
+    if _model is None:  
         current_dir = os.path.dirname(os.path.abspath(__file__))
         model_path = os.path.abspath(os.path.join(current_dir, "../../models/v1.2_Gradient_Boosting_calibrated.pkl"))
         
@@ -34,7 +34,23 @@ def test_model():
 
     print("Expected Class: 1.0")
     print("Predicted Class:", prediction[0])
+    
+    
+def test_model_json():
+    """Tests the model with a sample input."""
+    model = load_model()  
+    
+    sample_data = np.array([[0, 28, 104.8, 3046.05, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 
+                             1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 
+                             0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 
+                             0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0]])  # Expected class: 1
 
+    prediction = model.predict(sample_data)
+
+    return {
+        "Expected Class": 1.0, 
+        "Predicted Class":prediction[0]
+        }
 
 if __name__ == "__main__":
     test_model()
